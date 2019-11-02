9e810698b3590fda9aff3a85cebfd62dbbaf3a3b
echtniet <davelaarprogramming@gmail.com>
2019-11-02 12:39:55 -0500

    Initial Commit

---
+++ The Test's The ThingTests/The_Test_s_The_ThingTests.swift
@@ -0,0 +1,34 @@
+//
+//  The_Test_s_The_ThingTests.swift
+//  The Test's The ThingTests
+//
+//  Created by Student on 11/2/19.
+//  Copyright © 2019 Student. All rights reserved.
+//
+
+import XCTest
+@testable import The_Test_s_The_Thing
+
+class The_Test_s_The_ThingTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
